557ec0768dc30246eaa8d8c2bd1b0e4ec76cd5d8
vslakshmi03 <vslakshmi562@gmail.com>
2020-03-23 14:28:49 -0700

    subsets of all sets

---
+++ subsets_of_all_set.py
@@ -0,0 +1,10 @@
+class solution:
+    def powerset(self, sset):
+        return self.subsetsRecur([], sorted(sset))
+    
+    def subsetsRecur(self, current, sset):
+        if sset:
+            return self.subsetsRecur(current, sset[1:]) + self.subsetsRecur(current + [sset[0]], sset[1:])
+        return [current]
+
+print(solution().powerset([1,2,3]))
